7372c94ad139bcbdb4facb48f93cd06b600ae1eb
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-15 20:50:34 -0800

    1 - Model Tests

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.utils.text import slugify
 
+from .models import Post
 # Create your tests here.
+
+class PostModelTestCase(TestCase):
+	def setUp(self):
+		Post.objects.create(title="A new title", slug="this-is-unique-slug")
+
+	def test_post_title(self):
+		obj = Post.objects.get(slug="this-is-unique-slug")
+		self.assertEqual(obj.title, "A new title")
+		self.assertTrue(obj.content == "")
+
+	def create_post(self, title="This title"):
+		return Post.objects.create(title=title)
+
+	def test_post_slug(self):
+		# generate slug
+		title1 = "another title abc"
+		title2 = "another title abc"
+		slug1 = slugify(title1)
+		slug2 = slugify(title2)
+		obj1 = self.create_post(title=title1)
+		obj2 = self.create_post(title=title2)
+		self.assertEqual(obj1.slug, slug1)
+		self.assertNotEqual(obj2.slug, slug2)
+
+
+
+
+
